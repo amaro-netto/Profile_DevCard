1e36a4da62510325133b8140d260b20e5b90257d
amaro-netto <amarosilva002@gmail.com>
2025-07-16 14:16:56 -0300

    removendo componetes que não são mais utilizados e corrigindo imports

--- src/utils/statDisplayNames.js
+++
@@ -1,8 +0,0 @@
-// --- Mapeamento de nomes de estatísticas para exibição completa ---
-export const statDisplayNames = {
-  PWR: 'Poder',
-  VEL: 'Velocidade',
-  FLX: 'Flexibilidade',
-  COM: 'Comunicação',
-  CRV: 'Curva'
-};
