39a5b7f5edac4e5e6c9fa9a3a8c05700c693019f
amaro-netto <amarosilva002@gmail.com>
2025-07-16 15:59:15 -0300

    atualização de dependências

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('exibe mensagem para digitar nome de usuário', () => {
+test('exibe mensagem de erro quando campo está vazio e botão é clicado', () => {
   render(<App />);
-  const message = screen.getByText(/Digite um nome de usuário do GitHub/i);
+  const button = screen.getByText(/buscar/i);
+  fireEvent.click(button);
+  const message = screen.getByText(/Por favor, digite um nome de usuário do GitHub/i);
   expect(message).toBeInTheDocument();
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Importa o CSS global, incluindo Tailwind
+import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals'; // Pode remover se não for usar
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,7 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// Se quiser medir a performance da sua app, pode usar reportWebVitals
-// Para saber mais: https://bit.ly/CRA-vitals
-reportWebVitals();
